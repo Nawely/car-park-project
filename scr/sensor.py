74f467b4b561ad784f240c0bf089680be5d212e7
nawel youssouf <asukanawie@gmail.com>
2024-06-17 09:40:20 +0800

    Created core methods for the classes

--- scr/sensor.py
+++ scr/sensor.py
@@ -1,4 +1,8 @@
-class Sensor:
+from abc import ABC, abstractmethod
+import random
+
+
+class Sensor(ABC):
     def __init__(self, id, car_park,is_active=False):
         self.is_active = is_active
         self.id = id
@@ -7,11 +11,34 @@ class Sensor:
     def __str__(self):
         return f'{self.id}: Sensor is {"displaying" if self.is_active else "is not active"}'
 
+    def detect_vehicle(self):
+        plate = self._scan_plate()
+        self.update_car_park(plate)
+
+    @abstractmethod
+    def update_car_park(self, plate):
+        pass
+
+    def _scan_plate(self):
+        return 'FAKE-' + format(random.randint(0, 999), "03d")
+
 
 class EntrySensor(Sensor):
-        ...
+    # ... inside the EntrySensor class
+    def update_car_park(self, plate):
+        self.car_park.add_car(plate)
+        print(f"Incoming 🚘 vehicle detected. Plate: {plate}")
+
 
 class ExitSensor(Sensor):
-            ...
+
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates)
+
+    def update_car_park(self, plate):
+        self.car_park.remove_car(plate)
+        print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")
+
+
 
 
